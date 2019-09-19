089488cd0e6c50537cabb12418f91f5deb85ed44
claudiozollo <claudiozollo.seo@gmail.com>
2019-09-19 10:15:46 +0200

    Add files via upload

--- comandi.php
+++ comandi.php
@@ -1,8 +1,22 @@
 <?php
 
 if($msg == "/start") {
-sm($chatID, "Funziono!
-Ora modifica il file comandi.php");
+	sm($chatID, "Funziono!
+	Ora modifica il file comandi.php");
 }
+
+if($msg == "/mena"){
+	sm($chatID, "Devi specificare a quale utente cosi : /picchia nomeutente");
+}
+if($msg == "/mena" && $replyID){
+	
+	$utenti = array($name, $replyNome);
+
+	// Utilizzo la funzione array_rand per estrarre a caso uno degli elementi della array
+	$n = array_rand($utenti, 1);
+
+	sm($chatID, "Dopo un duro scontro il vincitore è : ".$n);
+}
+
 
 //altri comandi
