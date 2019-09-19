cb1635a43c9a01b868682e801146048063f0c6ef
claudiozollo <claudiozollo.seo@gmail.com>
2019-09-19 11:10:39 +0200

    Add files via upload

--- comandi.php
+++ comandi.php
@@ -5,17 +5,22 @@ if($msg == "/start" or $msg == "/start@picciobetabot") {
 	Ora baciatemi il culo");
 }
 
-if($msg == "/mena" && !$replyNome){
-	sm($chatID, "Devi rispondere ad un utente");
-}
-if($msg == "/mena" && $replyNome && $name){
+if($msg == "/mena"){
 	
-	$utenti = array($name, $replyNome);
+	
+	//se non c'è il nome in risposta
+	if(!$replyNome){
+		sm($chatID, "Devi rispondere ad un utente");
+	}
+	else{
+	
+		$utenti = array($name, $replyNome);
 
-	// Utilizzo la funzione array_rand per estrarre a caso uno degli elementi della array
-	$n = array_rand($utenti, 1);
+		// Utilizzo la funzione array_rand per estrarre a caso uno degli elementi della array
+		$n = array_rand($utenti, 1);
 
-	sm($chatID, "Dopo un duro scontro il vincitore è : ".$utenti[$n]);
+		sm($chatID, "Dopo un duro scontro il vincitore è : ".$utenti[$n]);
+	}
 }
 
 
