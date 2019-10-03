bd75d534c5fc61b65acc346080694b469c5eda86
claudiozollo <claudiozollo.seo@gmail.com>
2019-10-03 14:57:43 +0200

    Delete register.php

--- register.php
+++
@@ -1,8 +0,0 @@
-<?php
-$token = "938213190:AAHBvcVfWlL_mqLEEpBchQgsV7JxgVgwQcI"; //Il token da @BotFather
-$sito = "https://picciobot.herokuapp.com/index.php"; //Il percorso dell'index
-
-//////////////////////////////////// NON TOCCARE QUI ////////////////////////////////////////
-$webhook = 'https://api.telegram.org/bot'.$token.'/setwebhook?url='.$sito.'?api=bot'.$token;
-$ch = curl_init("$webhook");
-curl_exec($ch);
